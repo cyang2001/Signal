40dd71584d344acf4b3e369c499926505b5fb613
Chen YANG <gregyoungforever@gmail.com>
2023-11-23 13:10:58 +0100

    corrigé, mais pas encore testé

--- Semaine1_2/Problems1/src/functions/ProblemI.m
+++ Semaine1_2/Problems1/src/functions/ProblemI.m
@@ -6,41 +6,68 @@ function ProblemI()
   G = 30; % dB
   P_SPL = 80; % dB SPL
   D_t = 1; % s
-
+  sonStruct = struct('acceptable', struct(), 'penible', struct());
   for i = 1:length(signals)
       signal = signals{i};
       amplified_signal = FunctionAmplifier(signal, G);
-      spl_signal = ConvertToSPL(amplified_signal, S);
-      duree = length(signal); 
-      duree = duree*Ts{i};
-      power_mean_mW = mean(amplified_signal.^2)/1000;
-      power_mean_dBm = 10 * log10(power_mean_mW / 0.001);
-      rms = sqrt(power_mean_mW);
+      dureeTemp = 0;
+      n = 0;
+      j = 0;
+      sonTemp = [];
+      for tempSignal = amplified_signal
+          isLargerThandBm = false;
+          n = n + 1;
+          j = j + 1;
+          sonTemp = [sonTemp, tempSignal];
+          dureeTemp = dureeTemp + n*Ts{j};
+          if FunctionCalculerPowerMeandBM(tempSignal) >= FunctionConvertSPLTodBM(P_SPL, S)
+              isLargerThandBm = true;
+          end
+          if FunctionCalculerPowerMeandBM(tempSignal) < FunctionConvertSPLTodBM(P_SPL, S)
+              if isLargerThandBm && dureeTemp < D_t
+                FunctionSupport(i, dureeTemp, sonTemp, sonStruct, true);
+                dureeTemp = 0;
+                n = 0;
+                sonTemp = [];
+                sonTemp = [sonTemp, tempSignal];
+              end
+              if dureeTemp >= D_t && isLargerThandBm
+                FunctionSupport(i, dureeTemp, sonTemp, sonStruct, false);
+                dureeTemp = 0;
+                n = 0;
+                sonTemp = [];
+                sonTemp = [sonTemp, tempSignal];
+              end
+          end
+      end 
 
-      if duree > D_t && max(spl_signal) >= P_SPL
-          signal_class = 'son pénible';
-      else
-          signal_class = 'son acceptable';
-      end
-
-      resultats(i) = struct('duree', duree, 'power_mean_mW', power_mean_mW, ...
-                            'power_mean_dBm', power_mean_dBm, 'rms', rms, ...
-                            'signal_amplified', amplified_signal, 'signal_class', signal_class);
   end
-  for i=1:length(resultats)
-    fprintf('Signal %d:\n', i);
-    fprintf('  Durée: %.4f s\n', resultats(i).duree);
-    fprintf('  Puissance moyenne: %.4f mW (%.2f dBm)\n', resultats(i).power_mean_mW, resultats(i).power_mean_dBm);
-    fprintf('  RMS: %.4f V\n', resultats(i).rms);
-    fprintf('  Classe: %s\n', resultats(i).signal_class);
-    fprintf('\n');
+  
+end
+function FunctionSupport(i, dureeTemp, sonTemp, sonStruct,isAcceptable)
+  signalN = sprintf('signal%d', i);
+  duree = dureeTemp;
+  son = sonTemp(1:end-1);
+  p_mW = FunctionCalculerPowerMeanmW(son);
+  p_dBm = FunctionCalculerPowerMeandBM(son);
+  rms = sqrt(p_mW);
+  if isAcceptable
+    sonStruct.acceptable.(signalN) = struct('duree',duree,'puissance_mW', p_mW, 'puissance_dBm', p_dBm, 'Rms', rms,'son',son);
+  else
+    sonStruct.penible.(signalN) = struct('duree',duree,'puissance_mW', p_mW, 'puissance_dBm', p_dBm, 'Rms', rms,'son',son);
   end
 end
-
 function signal_amplified = FunctionAmplifier(signal, G)
   signal_amplified = signal * 10^(G/20);
 end
-function spl_signal = ConvertToSPL(signal, S)
-  reference = 1; 
-  spl_signal = 20 * log10(abs(signal) / reference) + (94 + S);
+function P_dbm = FunctionConvertSPLTodBM(P_SPL, S)
+  M_ref = 1;
+  P_reference = 20*10^(-6);
+  P_dbm = 10*log((M_ref*10^(S/20)*P_reference*10^(P_SPL/20)*30)^2*1000);
+end
+function power_mean_dBm = FunctionCalculerPowerMeandBM(signal)
+  power_mean_dBm = 10 * log10(FunctionCalculerPowerMeanmW(signal) / 0.001);
+end
+function power_mean_mW = FunctionCalculerPowerMeanmW(signal)
+  power_mean_mW = mean(signal.^2)/1000;
 end
